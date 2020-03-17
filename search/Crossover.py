a9e12ffa16af257f98afc434b7165eac3db5d78c
michal-swiatek <swiatek.michal006@gmail.com>
2020-03-17 17:50:34 +0100

    Adding Crossover base class Adding documentation to Mutation class

---
+++ search/Crossover.py
@@ -0,0 +1,15 @@
+"""
+    Created by michal-swiatek on 17.03.2020
+    Github: https://github.com/michal-swiatek
+"""
+
+
+class Crossover:
+    """
+        Representation of crossover
+
+        Crossover is a genetic operator responsible for
+    """
+
+    def cross(self, parent_a, parent_b):
+        pass
