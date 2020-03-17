5b275681cb21833be2674c8eea7d5c5af73ef462
RafalUzarowicz <rafal.uzarowicz@gmail.com>
2020-03-17 18:00:45 +0100

    - adding info

--- individual.py
+++ individual.py
@@ -1,1 +1,18 @@
-print("Individual")
+"""
+    Created by: Rafal Uzarowicz
+    Date: 17.03.2020
+    Github: https://github.com/RafalUzarowicz
+"""
+
+class Individual:
+    __gen = []
+
+    def getGen(self):
+        return self.__gen
+
+    def setGen(self, gen):
+        self.__gen = gen
+
+    def getFeno(self):
+        return self.__gen
+
