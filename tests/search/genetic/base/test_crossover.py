fed9bc9dee8b8834e5ba81eb1e4329f6e4f2e5db
michal-swiatek <swiatek.michal006@gmail.com>
2020-03-20 02:40:07 +0100

    Adding tests

---
+++ tests/search/genetic/base/test_crossover.py
@@ -0,0 +1,33 @@
+import unittest
+import random
+import copy
+
+import numpy as np
+
+import ai.search.genetic as ga
+
+
+class BaseCrossoverTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.crossover = ga.crossover.Crossover()
+
+        self.parent_a = np.array([random.randint(0, 10) for i in range(10)])
+        self.parent_b = np.array([random.randint(0, 10) for i in range(10)])
+
+        self.parent_a_copy = copy.deepcopy(self.parent_a)
+        self.parent_b_copy = copy.deepcopy(self.parent_b)
+
+    def testDefaultCross(self):
+        self.parent_a, self.parent_b = self.crossover.cross(self.parent_a, self.parent_b)
+
+        self.assertListEqual(list(self.parent_a), list(self.parent_a_copy))
+        self.assertListEqual(list(self.parent_b), list(self.parent_b_copy))
+
+    def testInplaceCross(self):
+        self.crossover.cross(self.parent_a, self.parent_b, inplace=True)
+
+        self.assertListEqual(list(self.parent_a), list(self.parent_a_copy))
+        self.assertListEqual(list(self.parent_b), list(self.parent_b_copy))
+
+
